29df8f7d80ae385dc2125af065dec08fe85b914f
Tianyun Yuan <7ianyun@berkeley.edu>
2023-05-01 17:49:13 -0700

    add UE5 STUFF

---
+++ UE5/Source/DroneSim/DroneSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DroneSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONESIM_API ADroneSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
